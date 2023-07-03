6167d945344d3161c010a2157555c93fd6ce1799
leksandre <44774042+leksandre@users.noreply.github.com>
2023-07-03 16:59:15 +0500

    commit2

---
+++ logintapSw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  logintapSw
+//
+//  Created by  Александр on 03.07.2023.
+//  Copyright © 2023  Александр. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
